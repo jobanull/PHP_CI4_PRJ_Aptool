a44429d5b55a1b159fdb01b73e716d9ee305de8e
LAPTOP-TKC3M1M2\jikhd <jikhdan@gmail.com>
2022-09-15 18:24:23 +0700

    Edit Profile

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -17,6 +17,10 @@ class Auth extends CI_Controller
 
     public function index()
     {
+        if ($this->session->userdata('email')) {
+            redirect('user');
+        }
+
         $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
         $this->form_validation->set_rules('password', 'password', 'trim|required');
         if ($this->form_validation->run() == false) {
@@ -70,6 +74,9 @@ class Auth extends CI_Controller
 
     public function registration()
     {
+        if ($this->session->userdata('email')) {
+            redirect('user');
+        }
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
         $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
             'is_unique' => 'This email has already registered!'
--- application/views/user/index.php
+++ application/views/user/index.php
@@ -3,10 +3,14 @@
 
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
-
-    <div class="card mb-3" style="max-width: 540px;">
+    <div class="row">
+        <div class="col-lg-6">
+            <?= $this->session->flashdata('message'); ?>
+        </div>
+    </div>
+    <div class="card mb-3 col-lg-6">
         <div class="row no-gutters">
-            <div class="col-md-4">
+            <div class="col-md-4 pb-3 pt-3">
                 <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="card-img">
             </div>
             <div class="col-md-8">
