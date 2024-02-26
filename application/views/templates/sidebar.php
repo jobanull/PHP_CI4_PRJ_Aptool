3ca52d9630f71a2a778c5d991a6fb937c668d55d
Joban <jikhdan@gmail.com>
2024-02-26 10:28:47 +0700

    Checkpoint

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -11,48 +11,42 @@
 
      <!-- Divider -->
      <hr class="sidebar-divider">
-     <?php
-        $role_id = $this->session->userdata('role_id');
-        $queryMenu = "SELECT `user_menu`.`id`, `menu`
-    FROM `user_menu`
-    JOIN `user_access_menu` ON `user_menu` . `id` = `user_access_menu` . `menu_id`
-   WHERE `user_access_menu` . `role_id` = $role_id
-   ORDER BY `user_access_menu` . `menu_id` ASC
-   ";
-        $menu = $this->db->query($queryMenu)->result_array();
-        ?>
-
-     <?php foreach ($menu as $m) : ?>
-
-         <div class="sidebar-heading">
-             <?= $m['menu'];  ?>
-         </div>
-         <?php
-            $menuId = $m['id'];
-            $querySubMenu = "SELECT * FROM `user_sub_menu` 
-         WHERE `menu_id` = $menuId
-        AND `is_active` = 1 
-        
-        ";
-
-            $subMenu = $this->db->query($querySubMenu)->result_array();
-            ?>
-
-         <?php foreach ($subMenu as $sm) : ?>
-            <?php if ($title == $sm['title']) :  ?>
-                 <li class="nav-item active">
-                 <?php else :  ?>
-                 <li class="nav-item ">
-                 <?php endif; ?>
-                 <a class="nav-link" href="<?= base_url($sm['url']); ?>">
-                     <i class="<?= $sm['icon']; ?>"></i>
-                     <span><?= $sm['title']; ?></span></a>
-             </li>
-
-
-         <?php endforeach; ?>
-         <hr class="sidebar-divider">
-     <?php endforeach; ?>
+    <div class="sidebar-heading">
+        <p>SURFACE</p>
+    </div>
+    <!-- Submenu items -->
+    <li class="nav-item">
+        <a class="nav-link" href="<?= base_url('surface/index'); ?>">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
+    <!-- Submenu items -->
+    <li class="nav-item">
+        <a class="nav-link"  href="<?= base_url('surface/tickets'); ?>">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Ticket</span>
+        </a>
+    </li>
+    <hr class="sidebar-divider">
+    <div class="sidebar-heading">
+        <p>Database Alat Ukur</p>
+    </div>
+    <!-- Submenu items -->
+    <li class="nav-item">
+        <a class="nav-link" href="<?= base_url('masterdata/alat_ukur'); ?>">
+            <i class="fas fa-fw fa-database"></i>
+            <span>Database Alat Ukur</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="<?= base_url('masterdata/alat_bantu'); ?>">
+            <i class="fas fa-fw fa-database"></i>
+            <span>Database Alat Bantu</span>
+        </a>
+    </li>
+    <hr class="sidebar-divider">
+    
 
 
      <li class="nav-item">
